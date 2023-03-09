0d910426241e9f0a0441c5a77b9c82807e995bca
Matthias Koch <ithrowexceptions@gmail.com>
2023-03-09 23:16:03 +0100

    Fix loading dependencies

--- source/Nuke.Build/Execution/BuildManager.cs
+++ source/Nuke.Build/Execution/BuildManager.cs
@@ -6,7 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
+using Microsoft.Extensions.DependencyModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities;
 using Nuke.Common.Utilities.Collections;
@@ -24,6 +26,14 @@ namespace Nuke.Common.Execution
         {
             add => s_cancellationHandlers.AddFirst(value);
             remove => s_cancellationHandlers.Remove(value);
+        }
+
+        [ModuleInitializer]
+        public static void Initialize()
+        {
+            DependencyContext.Default?.GetRuntimeAssemblyNames(string.Empty)
+                .Where(x => x.FullName.StartsWith("Nuke."))
+                .ForEach(x => AppDomain.CurrentDomain.Load(x));
         }
 
         public static int Execute<T>(Expression<Func<T, Target>>[] defaultTargetExpressions)
